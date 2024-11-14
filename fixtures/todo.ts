4da35389fe348f61f112e6f43ed49ad45e5f72c6
maguro-alternative <ka.ra.si.naporitan@gmail.com>
2024-11-14 09:50:12 +0900

    todoフィクスチャ追加

--- fixtures/todo.ts
+++ fixtures/todo.ts
@@ -1,5 +1,13 @@
-import { Fixture, ModelConnector, Todo } from './fixtures';
+import { Fixture, ModelConnector } from './fixtures';
 import { todos } from "../db/schema"
+
+export interface Todo {
+  id: number;
+  name: string;
+  done: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
 
 const newTodo = function(
   setter: ((Todo: Todo) => void)
@@ -8,7 +16,9 @@ const newTodo = function(
   const todo:Todo = {
     id: 0,
     name: "todo",
-    done: false
+    done: false,
+    createdAt: new Date(),
+    updatedAt: new Date()
   };
 
   return new ModelConnector(
@@ -31,7 +41,9 @@ const newTodo = function(
         .insert(todos)
         .values({
           name: todo.name,
-          done: todo.done
+          done: todo.done,
+          created_at: todo.createdAt,
+          updated_at: todo.updatedAt
         })
         .returning();
       todo.id = t[0].id;
