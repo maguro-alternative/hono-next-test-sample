4286a97767a06b5275981d72bcaa3059c9bf2692
maguro-alternative <ka.ra.si.naporitan@gmail.com>
2025-06-25 09:07:24 +0900

    rollback追加

--- app/api/[[...route]]/todo.test.ts
+++ app/api/[[...route]]/todo.test.ts
@@ -5,48 +5,43 @@ import { pool } from "../../../db/drizzle";
 
 import { fixtureBuild, newFixture } from "../../../fixtures/fixtures";
 import newTodo from '../../../fixtures/todo';
+import { withRollback } from '../../../utils/drizzle/testUtils';
 
 import { drizzle } from 'drizzle-orm/postgres-js';
 const db = drizzle(pool, { schema: { todos } })
 
 describe('GET /', () => {
   it('should return all todos', async () => {
-    try {
-      await db.transaction(async (tx) => {
-        let fixture = newFixture(tx);
-        fixture = await fixtureBuild(fixture,
-          newTodo(
-            (todo) => {
-              todo.name = '雪泉';
-              todo.done = false;
-            }
-          )
-        );
-        const res = await app.request('/api/todos', {}, {
-          DB: tx,
-        })
-        expect(res.status).toBe(200);
-        expect(await res.json()).toEqual({
-          todos: [
-            {
-              id: fixture.todos[0].id,
-              name: '雪泉',
-              done: false,
-              created_at: fixture.todos[0].createdAt.toJSON(),
-              updated_at: fixture.todos[0].updatedAt.toJSON()
-            }
-          ]
-        });
-        tx.rollback();
+    await withRollback(db, async (tx) => {
+      let fixture = newFixture(tx);
+      fixture = await fixtureBuild(fixture,
+        newTodo(
+          (todo) => {
+            todo.name = '雪泉';
+            todo.done = false;
+          }
+        )
+      );
+      const res = await app.request('/api/todos', {}, {
+        DB: tx,
+      })
+      expect(res.status).toBe(200);
+      expect(await res.json()).toEqual({
+        todos: [
+          {
+            id: fixture.todos[0].id,
+            name: '雪泉',
+            done: false,
+            created_at: fixture.todos[0].createdAt.toJSON(),
+            updated_at: fixture.todos[0].updatedAt.toJSON()
+          }
+        ]
       });
-    } catch (e) {
-      if (!(e instanceof TransactionRollbackError)) {
-        console.error(e);
-        throw new Error('Unexpected error');
-      }
-    }
+    });
   })
 });
+
+
 
 afterAll(() => {
   pool.end();
